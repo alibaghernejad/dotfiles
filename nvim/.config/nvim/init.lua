ad4628b1fa4d6e2a292e7adacff912eed926e8c9
a.baghernejad <alibaghernezhad@gmail.com>
2025-09-04 15:59:10 +0330

    Add line numbering defaults to nvim.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,31 @@
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = false
+
+-- [[ Setting options ]]
+-- See `:help vim.o`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
+
+-- Make line numbers default
+-- + Standard line numbering
+-- + Relative Line numbering
+-- + hybrid line Numbering (Use both of the standard and relative line numbering together)
+
+-- Standard line numbering
+vim.o.number = true -- short command: set nu/ set nonu
+
+-- Relative line numbering
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+vim.o.relativenumber = true -- set rnu/ set nornu
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
