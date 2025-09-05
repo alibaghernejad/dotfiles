44bcf941ed0346332c6ffb92a3d6e0b77beb61c2
a.baghernejad <alibaghernezhad@gmail.com>
2025-09-05 19:11:53 +0330

    Terminal Mode key-bindings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -52,6 +52,16 @@ end)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 -- Can run in command mode via `:lua vim.diagnostic.setloclist()` or `lopen`
 
+--===== Terminal Mode =====--
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
