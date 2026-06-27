5a8caba3b74677ca9c36bd50c8c66d09352f11b9
a.baghernejad <alibaghernezhad@gmail.com>
2026-06-27 17:21:01 +0330

    Update lsp configurations in coordinate to the latest nvim changes
    - Utilize new neovim APIs to config and enable LSPs
    - Add helper function to config and enable an LSP
    - Add a Lua table, which used by maison to ensure automatically LSP
    installation.

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,3 +1,13 @@
+local servers = {
+  "lua_ls",
+  "html",
+  "tailwindcss",
+  "ts_ls",
+  "solargraph",
+  "csharp_ls",
+  "fsautocomplete"
+
+}
 return {
   {
     "williamboman/mason.nvim",
@@ -10,7 +20,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     lazy = false,
     opts = {
-      auto_install = true,
+      ensure_installed = servers
     },
   },
   {
@@ -19,28 +29,46 @@ return {
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.tailwindcss.setup({
-        capabilities = capabilities
-      })
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
-      lspconfig.solargraph.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
+      -- Responsible to config and enable specified lsp.
+      local function setup (server, opts)
+        opts = opts or {}
+        opts.capabilities = capabilities
+
+        vim.lsp.config(server, opts);
+        vim.lsp.enable(server)
+      end
+
+      -- Automatically config and register LSPs
+      -- Feel free to add/remove your favorite LSPs to the `servers` table as pecified above.
+      for _, lsp in ipairs(servers) do
+       setup(lsp, {})
+      end
+
+      -- Add your custom LSP configuration with helper function here.
+      --
+      --setup("tailwindcss")
+      --setup("ts_ls")
+      --setup("csharp_ls")
+      --setup("fsautocomplete")
+     
+      -- Add your custom LSP configuration here.
+      --
+      --vim.lsp.config("tailwindcss", {
+      --  capabilities = capabilities
+      --})
+      --vim.lsp.enable("tailwindcss")
+
+      --vim.lsp.config("lua_ls", {
+      --  capabilities = capabilities
+      --})
+      --vim.lsp.enable("lua_ls")
+
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.code_action, {})
+      --vim.keymap.set("n", "<leader>rn", vim.lsp.buf.code_action, {})
       vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, {})
     end,
   },
